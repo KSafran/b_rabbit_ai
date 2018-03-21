34af0ff9ca929018283734bfde922df902af94c8
Kyle Safran <ksafran356@gmail.com>
2018-03-21 16:38:13 -0400

    debug home page

--- brabbit_django/brabbit_django/urls.py
+++ brabbit_django/brabbit_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from brabbit_django.rapper.generate_song import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$', home),
 ]
