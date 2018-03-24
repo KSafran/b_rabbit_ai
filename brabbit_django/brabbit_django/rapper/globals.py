e1ba6388d88a670daad0fc153ce9de88d663c703
Kyle Safran <ksafran356@gmail.com>
2018-03-24 12:35:18 -0400

    remove old django

--- brabbit_django/brabbit_django/rapper/globals.py
+++
@@ -1,4 +0,0 @@
-GENIUS_URL = "http://api.genius.com"
-with open('rapper/data/rap_genius_api.txt', 'r') as token_file:
-    TOKEN = token_file.read()
-HEADERS = {'Authorization': 'Bearer {}'.format(TOKEN)}
